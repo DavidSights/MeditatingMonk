2acdb9c3207da5273e79fbe940965e849f43ded5
David Seitz Jr <davidsights@me.com>
2019-04-01 20:00:17 -0500

    Code cleanup

--- Sources/Views/TipCloudNode.swift
+++ Sources/Views/TipCloudNode.swift
@@ -10,31 +10,31 @@ import Foundation
 
 class TipCloudNode: SKNode {
 
-    let tipLabel = SKLabelNode()
+    private let label = SKLabelNode()
 
     override init() {
         super.init()
         let tipCloudImage = SKSpriteNode(imageNamed: DeviceManager.isTablet ? "tipCloudiPad" : "tipCloudiPhone")
         addChild(tipCloudImage)
-        // TODO: Update font size for tablet.
-        setUpTipLabel(fontSize: DeviceManager.isTablet ? 10 : 10)
+        setUpLabel()
     }
 
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
 
-    private func setUpTipLabel(fontSize: CGFloat) {
-        tipLabel.fontSize = fontSize
-        tipLabel.fontName = FontManager.appFontName
-        tipLabel.position = CGPoint(x: 0, y: -5)
-        tipLabel.numberOfLines = 0
-        tipLabel.fontColor = Colors.tipLabelColor
-        addChild(tipLabel)
+    private func setUpLabel() {
+        // TOOD: Update font size for iPad.
+        label.fontSize =  DeviceManager.isTablet ? 10 : 10
+        label.fontName = FontManager.appFontName
+        label.position = CGPoint(x: 0, y: -5)
+        label.numberOfLines = 0
+        label.fontColor = Colors.tipLabelColor
+        addChild(label)
         reloadTip()
     }
 
     @objc func reloadTip() {
-        self.tipLabel.text = TipsArray.tip()
+        self.label.text = TipsArray.tip()
     }
 }
